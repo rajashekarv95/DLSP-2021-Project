5848915427cb49eb33faf166ee561f2363df8aac
rajashekarv95 <rajashekar.v95@gmail.com>
2021-04-09 22:59:42 -0400

    Bug fix

--- train_fixmatch.py
+++ train_fixmatch.py
@@ -44,7 +44,7 @@ def main():
     parser.add_argument('--learning-rate', type = float, default= 0.01)
     parser.add_argument('--threshold', type = float, default= 0.5)
     parser.add_argument('--mu', type= int, default= 7)
-    parser.add_argument('--lambda', type= int, default= 1)
+    parser.add_argument('--lambd', type= int, default= 1)
     args = parser.parse_args()
 
     print(torchvision.__version__, flush= True)
@@ -59,7 +59,7 @@ def main():
     threshold = args.threshold
     learning_rate = args.learning_rate
     momentum = 0.9
-    lamd = args.lambda
+    lamd = args.lambd
     tau = 0.95
     checkpoint_path = args.checkpoint_path
     train_from_start = args.train_from_start
