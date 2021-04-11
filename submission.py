6f615bc88e15a00f0edfa4cd7d3f496040a597b1
rajashekarv95 <rajashekar.v95@gmail.com>
2021-04-11 15:07:39 -0400

    Make resnet 18

--- submission.py
+++ submission.py
@@ -10,7 +10,7 @@ team_name = "Self Supervised Learners"
 email_address = "rv2138@nyu.edu"
 
 def get_model():
-    return resnet34(pretrained=False, num_classes = 800)
+    return resnet18(pretrained=False, num_classes = 800)
 
 mean = (0.4836, 0.4527, 0.4011)
 std = (0.3065, 0.2728, 0.2355)
