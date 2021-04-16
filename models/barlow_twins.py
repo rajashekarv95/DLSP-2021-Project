9bcdbdf5a62a741b9c3b1dab2ef37116c5573b18
rajashekarv95 <rajashekar.v95@gmail.com>
2021-04-16 11:12:29 -0400

    Modify linear layers

--- models/barlow_twins.py
+++ models/barlow_twins.py
@@ -9,7 +9,7 @@ class BarlowTwins(nn.Module):
 		self.backbone.fc = nn.Identity()
 
 		# projector
-		sizes = [2048] + [8192, 8192]
+		sizes = [512] + [8192, 8192, 8192]
 		layers = []
 		for i in range(len(sizes) - 2):
 			layers.append(nn.Linear(sizes[i], sizes[i + 1], bias=False))
