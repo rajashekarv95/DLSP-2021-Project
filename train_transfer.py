9180a094187ee48ff3f5d05ecd3e18f93c32d0b8
rajashekarv95 <rajashekar.v95@gmail.com>
2021-04-17 11:30:04 -0400

    Make bigger linear layer

--- train_transfer.py
+++ train_transfer.py
@@ -42,7 +42,8 @@ class Classifier(torch.nn.Module):
 		super().__init__()
 		self.fc1 = torch.nn.Linear(512, 1024)
 		self.fc2 = torch.nn.Linear(1024, 2048)
-		self.fc3 = torch.nn.Linear(2048, 800)
+		self.fc3 = torch.nn.Linear(2048, 4096)
+		self.fc4 = torch.nn.Linear(4096, 800)
 	
 
 	def forward(self, x):
@@ -52,6 +53,8 @@ class Classifier(torch.nn.Module):
 		x = self.fc2(x)
 		x = F.relu(x)
 		x = self.fc3(x)
+		x = F.relu(x)
+		x = self.fc4(x)
 
 		return x
 
