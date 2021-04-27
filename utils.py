2e6bf37dedbdf3cdb592307e18ec7839bc5bd251
sumitmamtani <sumitmamtani04@gmail.com>
2021-04-27 00:40:13 -0400

    updated utils.py

--- utils.py
+++ utils.py
@@ -5,10 +5,14 @@ import torch
 import yaml
 
 
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+# def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+#     torch.save(state, filename)
+#     if is_best:
+#         shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+def save_checkpoint(state,  filename):
     torch.save(state, filename)
-    if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')
 
 
 def save_config_file(model_checkpoints_folder, args):
