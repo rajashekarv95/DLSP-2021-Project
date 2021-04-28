49cd532a2b9a8f6ca36fb61ace7e44f4f87b3baa
sumitmamtani <sumitmamtani04@gmail.com>
2021-04-28 13:09:57 -0400

    added supervised training code

--- train_supervised.py
+++ train_supervised.py
@@ -141,8 +141,12 @@ def main():
 
 
         for batch_idx, batch in enumerate(tqdm(labeled_train_loader)):
+            print("batch[0] shape", batch[0].shape)
+            print("batch[1] shape", batch[1].shape)
             img_lab = torch.cat(batch[0], dim=0)
+            print("img_lab shape", img_lab.shape)
             targets_lab = torch.cat(batch[1], dim=0)
+            print("targets_lab shape", targets_lab.shape)
             img_lab = img_lab.to(device)
             targets_lab = targets_lab.to(device)
             logits_lab = model(img_lab)
