d3a2c6cbdfda57cd5156e2dd7fd1ed7fb18f5c71
Surabhi Ranjan <surabhiranjan@10-19-42-150.dynapool.wireless.nyu.edu>
2021-04-30 14:01:49 -0400

    classifer1

--- models/classifier.py
+++ models/classifier.py
@@ -8,10 +8,12 @@ class Classifier(torch.nn.Module):
 		self.fc1 = torch.nn.Linear(ip, 8192)
 		self.fc2 = torch.nn.Linear(8192, 8192)
 		self.fc3 = torch.nn.Linear(8192, 8192)
-		self.fc4 = torch.nn.Linear(8192, 800)
+		self.fc4 = torch.nn.Linear(8192, 8192)
+		self.fc5 = torch.nn.Linear(8192, 800)
 		self.bn1 = torch.nn.BatchNorm1d(8192)
 		self.bn2 = torch.nn.BatchNorm1d(8192)
 		self.bn3 = torch.nn.BatchNorm1d(8192)
+		self.bn4 = torch.nn.BatchNorm1d(8192)
 		self.dropout = torch.nn.Dropout(dp)
 
 	def forward(self, x):
@@ -25,6 +27,9 @@ class Classifier(torch.nn.Module):
 		x = self.bn3(self.fc3(x))
 		x = F.relu(x)
 		x = self.dropout(x)
-		x = self.fc4(x)
+		x = self.bn4(self.fc3(x))
+		x = F.relu(x)
+		x = self.dropout(x)
+		x = self.fc5(x)
 
 		return x
