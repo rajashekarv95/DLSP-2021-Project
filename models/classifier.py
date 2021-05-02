d25acbeec1bff0f8100307463e238e9ba9584d4d
rajashekarv95 <rajashekar.v95@gmail.com>
2021-05-01 20:14:34 -0400

    Make classifier smaller

--- models/classifier.py
+++ models/classifier.py
@@ -6,21 +6,13 @@ class Classifier(torch.nn.Module):
 	def __init__(self, ip, dp):
 		super().__init__()
 		self.fc1 = torch.nn.Linear(ip, 8192)
-		self.fc2 = torch.nn.Linear(8192, 8192)
+		self.fc2 = torch.nn.Linear(8192, 800)
 		self.fc3 = torch.nn.Linear(8192, 8192)
 		self.fc4 = torch.nn.Linear(8192, 8192)
-		#New
-		self.fc5 = torch.nn.Linear(8192, 8192)
-		self.fc6 = torch.nn.Linear(8192, 800)
-		#New
 		self.bn0 = torch.nn.BatchNorm1d(ip)
 		self.bn1 = torch.nn.BatchNorm1d(8192)
 		self.bn2 = torch.nn.BatchNorm1d(8192)
 		self.bn3 = torch.nn.BatchNorm1d(8192)
-		#New
-		self.bn4 = torch.nn.BatchNorm1d(8192)
-		self.bn5 = torch.nn.BatchNorm1d(8192)
-		#New
 		self.dropout = torch.nn.Dropout(dp)
 
 	def forward(self, x):
@@ -29,18 +21,18 @@ class Classifier(torch.nn.Module):
 		x = self.bn1(self.fc1(x))
 		x = F.relu(x)
 		x = self.dropout(x)
-		x = self.bn2(self.fc2(x))
-		x = F.relu(x)
-		x = self.dropout(x)
-		x = self.bn3(self.fc3(x))
-		x = F.relu(x)
-		x = self.dropout(x)
-		x = self.bn4(self.fc4(x))
-		x = F.relu(x)
-		x = self.dropout(x)
-		x = self.bn5(self.fc5(x))
-		x = F.relu(x)
-		x = self.dropout(x)
-		x = self.fc6(x)
+		# x = self.bn2(self.fc2(x))
+		# x = F.relu(x)
+		# x = self.dropout(x)
+		# x = self.bn3(self.fc3(x))
+		# x = F.relu(x)
+		# x = self.dropout(x)
+		# x = self.bn4(self.fc4(x))
+		# x = F.relu(x)
+		# x = self.dropout(x)
+		# x = self.bn5(self.fc5(x))
+		# x = F.relu(x)
+		# x = self.dropout(x)
+		x = self.fc2(x)
 
 		return x
